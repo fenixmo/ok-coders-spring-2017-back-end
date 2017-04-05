e74bc25cfc138601920dee67fc9bd3326f0c7c00
fenixmo <fenixpiconi@gmail.com>
2017-04-04 23:38:34 -0500

    finished week 2 exercises

--- exercises/week-2/collections/node/objects.js
+++ exercises/week-2/collections/node/objects.js
@@ -9,14 +9,17 @@
 // 1.
 // Create an object with a `name` property whose value is 'Michael'
 
-var obj1 = { }
+var obj1 = { name: 'Michael'}
 
 
 // 2.
 // Create an object with a `name` property whose value is 'James'
 // and and `age` property with the value 48
 
-var obj2 = { }
+var obj2 = {
+  name: 'James',
+  age: 48
+}
 
 
 // 3.
@@ -25,7 +28,7 @@ var obj2 = { }
 // to define the object slightly differently.
 // See lecture notes for help
 
-var obj3 = { }
+var obj3 = {'happy-days': true }
 
 
 // 4.
@@ -34,7 +37,7 @@ var obj3 = { }
 var obj4 = {
   name: 'Carissa',
   age: 23,
-  music: 'Country'
+  music: 'Techno'
 }
 
 
@@ -43,7 +46,12 @@ var obj4 = {
 // The name object should have `first` and `last` properties whose
 // values are 'Ada' and 'Lovelace' respectively.
 
-var obj5 = { }
+var obj5 = {
+  name: {
+    first: 'Ada',
+    last: 'Lovelace'
+  }
+}
 
 
 // 6.
@@ -55,7 +63,16 @@ var obj5 = { }
 // values are 'Creative Director' and 'NeXT' respectively.
 // Watch the capitalization and syntax!
 
-var obj6 = { }
+var obj6 = {
+  name: {
+    first: 'Susan',
+    last: 'Kare',
+  },
+  job: {
+    title: 'Creative Director',
+    company: 'NeXT'
+  }
+}
 
 
 // 7.
@@ -68,7 +85,11 @@ var func = function() {
   // do something
 }
 
-var obj7 = { }
+var obj7 = {
+  meaningOfLife: function () {
+    return 42
+  }
+}
 
 
 // 8.
@@ -78,7 +99,14 @@ var obj7 = { }
 // e.g. successMsg = 'Completed' or failureMsg = 'Incomplete'
 // Don't redeclare the variables!
 
-var obj8 = { }
+var obj8 = {
+  completed: function () {
+    successMsg = 'Completed'
+  },
+  failed: function () {
+    failureMsg = 'Failed'
+  }
+}
 
 // do not modify the following code for problem 8, but examine it
 // in order to under how it works. make sure you understand the control flow
@@ -108,7 +136,11 @@ fail(obj8)
 // Create an object with single function `quadruple` which takes a single numeric
 // paramater and returns its value multipled by 4
 
-var obj9 = { }
+var obj9 = {
+  quadruple: function (num) {
+    return num * 4
+  }
+}
 
 
 
